8cef701f4414caff6b4af0fd5d9b1edf0fdd5508
Shikhar Mandloi <shikhar.mandloi@gmail.com>
2024-06-11 13:42:29 +0530

    fix: comment updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-// Export only requried modules src/index.ts
+// Export only requried modules :src/index.ts
 export { default as useFetch } from "./useFetch";
 export { default as useMutation } from "./useMutation";
